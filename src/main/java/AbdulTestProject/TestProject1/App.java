547244e8377c7604d677885e1af0007ae564f314
Abdul <uha24278@gmail.com>
2024-07-04 16:14:44 +0530

    initial file commit

---
+++ src/main/java/AbdulTestProject/TestProject1/App.java
@@ -0,0 +1,13 @@
+package AbdulTestProject.TestProject1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/AbdulTestProject/TestProject1/AppTest.java
@@ -0,0 +1,20 @@
+package AbdulTestProject.TestProject1;
+
+import org.testng.*;
+import org.testng.annotations.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+       System.out.println("Answer should be True Always");
+    }
+}
